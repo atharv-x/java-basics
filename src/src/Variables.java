fd574a012653b9520f7027e668b5cf1b4be07728
Atharv <95841903+atharv-x@users.noreply.github.com>
2026-02-28 09:00:15 +0530

    Add variables example

---
+++ src/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
