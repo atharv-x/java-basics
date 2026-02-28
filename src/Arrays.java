201dff8508d7fed3a0b50107d1244006e62ef94d
Atharv <95841903+atharv-x@users.noreply.github.com>
2026-02-28 09:07:24 +0530

    Add arrays example

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
